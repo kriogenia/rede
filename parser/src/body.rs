10be93ab1da099a268ee63a0634240fba2e114ec
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2024-03-25 06:35:23 +0000

    feat: add form body types

--- parser/src/body.rs
+++ parser/src/body.rs
@@ -1,7 +1,16 @@
 use serde::Deserialize;
+use toml::map::Map;
+use toml::Value;
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(untagged)]
+pub(super) enum OptionalBody {
+    Body(Body),
+    Empty {},
+}
 
 #[derive(Debug, Default, Deserialize, PartialEq)]
-#[serde(rename_all = "lowercase")]
+#[serde(rename_all = "snake_case")]
 pub enum Body {
     #[default]
     None,
@@ -9,6 +18,23 @@ pub enum Body {
     Raw(String),
     #[serde(alias = "file")]
     Binary(String),
+    #[serde(alias = "form-data", alias = "formdata")]
+    FormData(Map<String, Value>),
+    #[serde(
+        alias = "x-www-form-urlencoded",
+        alias = "form_urlencoded",
+        alias = "form-urlencoded"
+    )]
+    XFormUrlEncoded(Map<String, Value>),
+}
+
+impl From<OptionalBody> for Body {
+    fn from(value: OptionalBody) -> Self {
+        match value {
+            OptionalBody::Empty {} => Body::None,
+            OptionalBody::Body(body) => body,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -17,14 +43,39 @@ mod test {
 
     #[derive(Deserialize)]
     struct Parent {
-        body: Body,
+        body: OptionalBody,
+    }
+
+    impl Deref for OptionalBody {
+        type Target = Body;
+
+        fn deref(&self) -> &Self::Target {
+            match self {
+                OptionalBody::Empty {} => &Body::None,
+                OptionalBody::Body(body) => body,
+            }
+        }
     }
 
     #[test]
     fn deserialize() {
-        let toml = r#"body.raw = "content""#;
-        let parent: Parent = toml::from_str(toml).unwrap();
-        assert_eq!(parent.body, Body::Raw("content".to_string()));
+        assert_eq!(
+            *toml::from_str::<Parent>("[body]").unwrap().body,
+            Body::None
+        );
+        assert_eq!(
+            *toml::from_str::<Parent>(r#"body.raw = "content""#)
+                .unwrap()
+                .body,
+            Body::Raw("content".to_string())
+        );
+        let toml = r#"body.form-data = { type = "integer", value = 1 }"#;
+        let body = toml::from_str::<Parent>(toml).unwrap().body;
+        assert!(matches!(&*body, Body::FormData(map) if map.len() == 2));
+        if let Body::FormData(map) = &*body {
+            assert_eq!(map["type"], Value::String("integer".to_string()));
+            assert_eq!(map["value"], Value::Integer(1));
+        }
     }
 
     #[test]
