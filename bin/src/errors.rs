134862e0f9cbbbff20edb7231def81d78f45fb3b
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2024-04-05 21:26:59 +0100

    feat: parse request

--- bin/src/errors.rs
+++ bin/src/errors.rs
@@ -1,21 +1,54 @@
-use miette::Diagnostic;
+use miette::{Diagnostic, SourceSpan};
 use thiserror::Error;
 
 #[derive(Error, Debug, Diagnostic)]
 pub enum InnerError {
     #[error("Failed to read {filename}")]
-    #[diagnostic(help("check if the file name is correct or you're in the correct path"))]
-    IoError {
+    #[diagnostic(
+        code(io),
+        help("check if the file name is correct or you're in the correct path")
+    )]
+    IO {
         filename: String,
         source: std::io::Error,
     },
+    #[error("{message}")]
+    #[diagnostic(
+        code(parsing),
+        url("https://toml.io/en/v1.0.0"),
+        help("check the TOML specification and rede schema if you don't know what is wrong")
+    )]
+    Parsing {
+        message: String,
+        #[source_code]
+        code: String,
+        #[label("here")]
+        span: Option<SourceSpan>,
+    },
+    #[error(transparent)]
+    #[diagnostic(
+        code(typing),
+        help("usually keys accept only strings or everything except datetimes and tables")
+    )] // todo: url to schema specification
+    WrongType { source: rede_parser::Error },
 }
 
 impl InnerError {
     pub fn io<T: Into<String>>(filename: T, source: std::io::Error) -> Self {
-        Self::IoError {
+        Self::IO {
             filename: filename.into(),
             source,
+        }
+    }
+
+    pub fn parsing<T: Into<String>>(code: T, source: rede_parser::Error) -> Self {
+        match source {
+            rede_parser::Error::InvalidFile(e) => InnerError::Parsing {
+                message: e.message().to_owned(),
+                code: code.into(),
+                span: e.span().map(SourceSpan::from),
+            },
+            rede_parser::Error::InvalidType { .. } => InnerError::WrongType { source },
         }
     }
 }
