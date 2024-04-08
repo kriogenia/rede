516617293c1034f80089d1fce552b5cbb8327917
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2024-04-08 19:11:11 +0100

    feat: define primitive array type

--- parser/src/schema/types.rs
+++ parser/src/schema/types.rs
@@ -12,6 +12,14 @@ pub(crate) enum Primitive {
     Str(String),
 }
 
+/// Subset with the four primitive types and the array of those
+#[derive(Clone, Debug, Deserialize, PartialEq)]
+#[serde(untagged)]
+pub(crate) enum PrimitiveArray {
+    Single(Primitive),
+    Multiple(Vec<Primitive>),
+}
+
 impl From<Primitive> for String {
     fn from(value: Primitive) -> Self {
         match value {
@@ -19,6 +27,19 @@ impl From<Primitive> for String {
             Primitive::Bool(b) => b.to_string(),
             Primitive::Float(f) => f.to_string(),
             Primitive::Int(i) => i.to_string(),
+        }
+    }
+}
+
+impl From<PrimitiveArray> for String {
+    fn from(value: PrimitiveArray) -> Self {
+        match value {
+            PrimitiveArray::Single(p) => p.into(),
+            PrimitiveArray::Multiple(a) => a
+                .into_iter()
+                .map(String::from)
+                .collect::<Vec<String>>()
+                .join(","),
         }
     }
 }
@@ -31,19 +52,36 @@ mod test {
 
     #[derive(Deserialize)]
     struct Parent<T>(HashMap<String, T>);
-    type PParent = Parent<Primitive>;
+    type Pri = Parent<Primitive>;
+    type PriArr = Parent<PrimitiveArray>;
 
     #[test]
     fn primitive() {
-        assert_eq!(parse(r#"primitive="string""#), "string");
-        assert_eq!(parse(r#"primitive=10"#), "10");
-        assert_eq!(parse(r#"primitive=5.1"#), "5.1");
-        assert_eq!(parse(r#"primitive=true"#), "true");
-        assert!(toml::from_str::<PParent>(r#"a=1970-01-01"#).is_err());
+        assert_eq!(parse_pri(r#"primitive="string""#), "string");
+        assert_eq!(parse_pri(r#"primitive=10"#), "10");
+        assert_eq!(parse_pri(r#"primitive=5.1"#), "5.1");
+        assert_eq!(parse_pri(r#"primitive=true"#), "true");
+        assert!(toml::from_str::<Pri>(r#"a=1970-01-01"#).is_err());
     }
 
-    fn parse(str: &str) -> String {
-        toml::from_str::<PParent>(str).unwrap().0["primitive"]
+    #[test]
+    fn primitive_array() {
+        assert_eq!(parse_pri_arr(r#"primitive="string""#), "string");
+        assert_eq!(parse_pri_arr(r#"primitive=10"#), "10");
+        assert_eq!(parse_pri_arr(r#"primitive=5.1"#), "5.1");
+        assert_eq!(parse_pri_arr(r#"primitive=true"#), "true");
+        assert_eq!(parse_pri_arr(r#"primitive=[1,"two"]"#), "1,two");
+        assert!(toml::from_str::<PriArr>(r#"a=1970-01-01"#).is_err());
+    }
+
+    fn parse_pri(str: &str) -> String {
+        toml::from_str::<Pri>(str).unwrap().0["primitive"]
+            .clone()
+            .into()
+    }
+
+    fn parse_pri_arr(str: &str) -> String {
+        toml::from_str::<PriArr>(str).unwrap().0["primitive"]
             .clone()
             .into()
     }
