cff6cf62088b18b68b51f418af58e0fb54d49785
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2024-04-10 21:10:02 +0100

    test: improve run tests

--- bin/tests/run.rs
+++ bin/tests/run.rs
@@ -5,34 +5,8 @@ use predicates::prelude::predicate::str::contains;
 // Set of integration tests for `rede run`, they are all ignored to run them only manually.
 // The majority of these tests are built against a custom test API
 
-macro_rules! test_request {
-    ($name:ident -> $assert:expr) => {
-        #[test]
-        #[ignore]
-        fn $name() {
-            let file = format!("tests/inputs/{}", stringify!($name));
-            Command::cargo_bin("rede")
-                .unwrap()
-                .arg("run")
-                .arg(file)
-                .assert()
-                .success()
-                .stdout($assert);
-        }
-    };
-}
-
-macro_rules! test_error {
-    // Runs the test using the file matching the test name
-    ($(#[$m:meta])*$name:ident $(, $arg:literal)* -> $assert:expr) => {
-        test_error!($(#[$m])*$name <$name>, $($arg),* -> $assert);
-    };
-    // Runs the test using the `get_simple` file, for test not dependent on the request file
-    ($(#[$m:meta])*$name:ident <>, $($arg:literal),* -> $assert:expr) => {
-        test_error!($(#[$m])*$name <get_simple>, $($arg),* -> $assert);
-    };
-    // Runs the test using the given file
-    ($(#[$m:meta])*$name:ident <$file:ident>, $($arg:literal),* -> $assert:expr) => {
+macro_rules! test_req {
+    ($(#[$m:meta])*$name:ident, $result:ident, $std:ident, <$file:ident> $($arg:literal),* -> $assert:expr) => {
         $(#[$m])*
         #[test]
         fn $name() {
@@ -43,9 +17,33 @@ macro_rules! test_error {
                 $(.arg($arg))*
                 .arg(file)
                 .assert()
-                .failure()
-                .stderr($assert);
+                .$result()
+                .$std($assert);
         }
+    };
+}
+
+macro_rules! test_request {
+    ($name:ident -> $assert:expr) => {
+        test_req!(#[ignore]$name, success, stdout, <$name> -> $assert);
+    };
+    ($name:ident <$file:ident> -> $assert:expr) => {
+        test_req!(#[ignore]$name, success, stdout, <$file> -> $assert);
+    };
+}
+
+macro_rules! test_error {
+    // Runs the test using the file matching the test name
+    ($(#[$m:meta])*$name:ident $(, $arg:literal)* -> $assert:expr) => {
+        test_req!($(#[$m])*$name, failure, stderr, <$name> $($arg),* -> $assert);
+    };
+    // Runs the test using the `get_simple` file, for test not dependent on the request file
+    ($(#[$m:meta])*$name:ident <> $($arg:literal),* -> $assert:expr) => {
+        test_req!($(#[$m])*$name, failure, stderr, <get_simple> $($arg),* -> $assert);
+    };
+    // Runs the test using the given file
+    ($(#[$m:meta])*$name:ident <$file:ident> $($arg:literal),* -> $assert:expr) => {
+        test_req!($(#[$m])*$name, failure, stderr, <$file> $($arg),* -> $assert);
     };
 }
 
@@ -65,7 +63,7 @@ test_error!(invalid_url -> contains("invalid url").and(contains("http://128.0.0.
 test_error!(failed_connection -> contains("failed connection").and(contains("completelymadeupurl")));
 test_error!(bad_url_scheme -> contains("failed request building").and(contains("htt:/www.url.com")));
 test_error!(wrong_binary -> contains("invalid file").and(contains("no_exists.zip")));
-test_error!(timeout<>, "--timeout", "0ms" -> contains("timeout"));
 
+test_error!(#[ignore] timeout<> "--timeout", "0ms" -> contains("timeout"));
 test_error!(#[ignore] unsupported_http_version -> contains("wrong http version"));
 test_error!(#[ignore] redirect_loop, "--max-redirects", "5" -> contains("redirect"));
