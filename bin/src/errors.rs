77b3e97e0dcf51520dd268c302a5b9f95d783cba
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2024-04-14 16:46:56 +0100

    doc: add links to documentation web

--- bin/src/errors.rs
+++ bin/src/errors.rs
@@ -10,7 +10,7 @@ pub enum ParsingError {
     #[error("{message}")]
     #[diagnostic(
         code("spec violation"),
-        url("https://toml.io/en/v1.0.0"), // todo change with Request DSL page
+        url("https://rede.sotoestevez.dev/reference_guide/request_dsl.html"),
         help("check the TOML specification and rede schema if you don't know what is wrong")
     )]
     Deserialization {
@@ -23,6 +23,7 @@ pub enum ParsingError {
     #[error("Failed to read {}", style(filename).yellow())]
     #[diagnostic(
         code("invalid [REQUEST]"),
+        url("https://rede.sotoestevez.dev/reference_guide/errors.html#invalid-request"),
         help("check if the file name is correct or you're in the correct path")
     )]
     IO { filename: String, source: IOError },
@@ -31,42 +32,56 @@ pub enum ParsingError {
 #[derive(Debug, Diagnostic, Error)]
 pub enum RequestError<E: Error> {
     #[error(transparent)]
-    #[diagnostic(code = "failed request building")]
+    #[diagnostic(
+        code = "failed request building",
+        url = "https://rede.sotoestevez.dev/reference_guide/errors.html#failed-request-building"
+    )]
     Building(E),
     #[error(transparent)]
-    #[diagnostic(code = "failed connection")]
+    #[diagnostic(
+        code = "failed connection",
+        url = "https://rede.sotoestevez.dev/reference_guide/errors.html#failed-connection"
+    )]
     FailedConnection(E),
     #[error("resulting url is not correct ({})", style(url).underlined().cyan())]
-    #[diagnostic(code("invalid url"))]
+    #[diagnostic(
+        code = "invalid url",
+        url = "https://rede.sotoestevez.dev/reference_guide/errors.html#invalid-url"
+    )]
     InvalidUrl {
         url: String,
         source: url::ParseError,
     },
     #[error("a file defined on the request could not be loaded: {}", style(filename).yellow())]
     #[diagnostic(
-        code("invalid file"),
+        code = "invalid file",
+        url = "https://rede.sotoestevez.dev/reference_guide/errors.html#invalid-file",
         help(
             "request file paths must be relative to the directory where `rede` is being executed"
         )
     )]
     IO { filename: String, source: IOError },
     #[error(transparent)]
-    #[diagnostic(code("redirect"))]
+    #[diagnostic(
+        code = "redirect",
+        url = "https://rede.sotoestevez.dev/reference_guide/errors.html#redirect"
+    )]
     Redirect(E),
     #[error(transparent)]
-    #[diagnostic(code("timeout"))]
+    #[diagnostic(code = "timeout")]
     Timeout(E),
     #[error(transparent)]
     #[diagnostic(
-        code("wrong http version"),
-        help("maybe that port or endpoint does not support this protocol version")
+        code = "wrong http version",
+        url = "https://rede.sotoestevez.dev/reference_guide/errors.html#wrong-http-version",
+        help = "maybe that port or endpoint does not support this protocol version"
     )]
     WrongVersion(E),
     #[error(transparent)]
     #[diagnostic(
-        code("unknown request error"),
-        url("https://github.com/kriogenia/rede/issues"),
-        help("if you contact with the development team with the error we could start tracking it")
+        code = "unknown request error",
+        url = "https://github.com/kriogenia/rede/issues",
+        help = "if you contact with the development team with the error we could start tracking it"
     )]
     Unknown(E),
 }
