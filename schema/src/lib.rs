f1a82dd7afdef7d39ee2dd95932f6c515b55eaa1
SotoEstévez <ricardo@sotoestevez.dev>
2024-09-27 16:43:47 +0200

    refactor: move input params to schema crate

--- schema/src/lib.rs
+++ schema/src/lib.rs
@@ -5,5 +5,12 @@
 /// Contains all the specific types used in the body
 pub mod body;
 
+#[cfg(feature = "input_params")]
+mod input_param;
+
 #[doc(inline)]
 pub use body::Body;
+
+#[cfg(feature = "input_params")]
+#[doc(inline)]
+pub use input_param::InputParam;
