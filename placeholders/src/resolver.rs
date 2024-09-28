5b462bcf76d13ac3ef93f8b5a06a59911011e8db
SotoEstévez <ricardo@sotoestevez.dev>
2024-09-28 22:04:46 +0200

    refactor: resolve return value

--- placeholders/src/resolver.rs
+++ placeholders/src/resolver.rs
@@ -5,7 +5,6 @@ use rede_schema::Request;
 use crate::{Placeholders, ValuePicker};
 
 // TODO remove the request from the ValuePicker trait
-// TODO return an structure with the values instead of the Result nomad (or map with options?)
 
 #[derive(Default)]
 pub struct Resolver {
@@ -39,8 +38,8 @@ impl Resolver {
     /// # use crate::rede_placeholders::value_picker::{VariablesPicker, EnvVarPicker};
     /// #
     /// let toml = r#"
-    /// http = { url = "http://localhost:8080/{{name}}", method = "GET" }
-    /// variables = { name = "variable "}
+    /// http = { url = "http://localhost:8080/{{name}}/{{unresolved}}", method = "GET" }
+    /// variables = { name = "variable" }
     /// "#;
     /// let request = rede_parser::parse_request(toml).unwrap();
     /// let placeholders = (&request).into();
@@ -48,36 +47,74 @@ impl Resolver {
     /// let mut resolver = Resolver::new();
     /// &resolver.add_picker(Box::new(EnvVarPicker)).add_picker(Box::new(VariablesPicker));
     ///
-    /// let values = resolver.resolve(&placeholders, &request).expect("All placeholders resolved");
-    /// assert_eq!(values["name"], request.variables["name"]);
+    /// let ph_values = resolver.resolve(&placeholders, &request);
+    /// assert_eq!(ph_values.values["name"], Some("variable".to_string()));
+    /// assert_eq!(ph_values.values["unresolved"], None);
     ///
-    /// std::env::set_var("name", "value");
-    /// let values = resolver.resolve(&placeholders, &request).expect("All placeholders resolved");
-    /// assert_ne!(values["name"], request.variables["name"]);
-    /// assert_eq!(values["name"], "value");
+    /// std::env::set_var("name", "env_var");
+    /// std::env::set_var("unresolved", "fixed");
+    /// let ph_values = resolver.resolve(&placeholders, &request);
+    /// assert_ne!(ph_values.values["name"], Some("variable".to_string()));
+    /// assert_eq!(ph_values.values["name"], Some("env_var".to_string()));
+    /// assert_eq!(ph_values.values["unresolved"], Some("fixed".to_string()));
     /// ```
-    ///
-    /// # Errors
-    ///
-    /// // TODO to be removed
-    pub fn resolve<'a>(
+    #[must_use]
+    pub fn resolve<'ph>(
         &self,
-        placeholders: &'a Placeholders,
+        placeholders: &'ph Placeholders,
         req: &Request,
-    ) -> Result<HashMap<&'a str, String>, Vec<&'a str>> {
+    ) -> PlaceholderValues<'ph> {
+        let values: HashMap<&str, Option<String>> = placeholders
+            .keys()
+            .map(|k| (k, self.pickers.iter().find_map(|p| p.pick_for(req, k))))
+            .collect();
+        PlaceholderValues { values }
+    }
+}
+
+/// Wrapper over the resolved values for the placeholders. You can directly access its internal
+/// `values` or use the convinient methods to ease access to its content.
+#[derive(Debug)]
+pub struct PlaceholderValues<'ph> {
+    // Contains the collection placeholders and their values after from the `resolve` call.
+    pub values: HashMap<&'ph str, Option<String>>,
+}
+
+impl<'ph> PlaceholderValues<'ph> {
+    // Returns an iterator with the pairs of placeholder keys and their resolved values found.
+    // Every placeholder unresolved won't be returned in this iterator, but in the `unresolved` one.
+    pub fn resolved(&self) -> impl Iterator<Item = (&str, String)> {
+        self.values
+            .iter()
+            .filter(|(_, v)| v.is_some())
+            .map(|(k, v)| (*k, v.clone().unwrap()))
+    }
+
+    // Returns an iterator with the placeholder keys that ended up unresolved.
+    // Every placeholder resolved won't be returned in this iterator, but in the `resolved` one.
+    pub fn unresolved(&self) -> impl Iterator<Item = &str> {
+        self.values
+            .iter()
+            .filter(|(_, v)| v.is_none())
+            .map(|(k, _)| *k)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+
+    use super::PlaceholderValues;
+
+    #[test]
+    fn placeholder_values_iters() {
         let mut values = HashMap::new();
-        let mut missing = Vec::new();
-        for k in placeholders.keys() {
-            if let Some(value) = self.pickers.iter().find_map(|p| p.pick_for(req, k)) {
-                values.insert(k, value);
-            } else {
-                missing.push(k);
-            }
-        }
-        if missing.is_empty() {
-            Ok(values)
-        } else {
-            Err(missing)
-        }
+        values.insert("resolved", Some("value".to_string()));
+        values.insert("unresolved", None);
+        values.insert("resolved_too", Some("different_value".to_string()));
+
+        let ph_values = PlaceholderValues { values };
+        assert_eq!(ph_values.resolved().count(), 2);
+        assert!(ph_values.unresolved().all(|k| k == "unresolved"));
     }
 }
